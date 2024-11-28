11b27fcfe42174db213eed2dc7b1f7b1f17c8988
Abhiram-Laha <abhiramlaha2020@gmail.com>
2024-11-29 00:00:00 +0530

    ok

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template
 import numpy as np
 import pickle
 from flask_session import Session  
