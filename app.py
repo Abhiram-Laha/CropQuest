b997e9e0294dd5db26434c63ab39f87a1e1b1cbb
Abhiram-Laha <abhiramlaha2020@gmail.com>
2024-12-13 14:46:55 +0530

    ok

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for
 import numpy as np
 import pickle
 from flask_session import Session  
 
-model_crop = pickle.load(open('Models\RandomForest.pkl', 'rb'))
+model_crop = pickle.load(open('Models/RandomForest.pkl', 'rb'))
+model = pickle.load(open('Models/classifier.pkl', 'rb'))
+ferti = pickle.load(open('Models/fertilizer.pkl', 'rb'))
 
 
 app = Flask(__name__)
 
-
+app.secret_key = 'your_secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'  
 Session(app)  
 
@@ -16,6 +18,8 @@ Session(app)
 def home():
     return render_template('index.html')
 
+
+#? 1. CROP RECOMMENDATION
 
 @app.route('/crop-recommend')
 def crop_recommendation():
@@ -69,6 +73,44 @@ def predict_crop():
         result = "Error occurred, please check the input values."
 
     return render_template('crop_rec.html', result=result, image_filename=image_filename)
+
+
+#? 2. Fertilizer Suggestion
+
+@app.route('/fertilizer-suggestion')
+def fertilizer_suggestion():
+    x = session.get('fertilizer_result', None) 
+    return render_template('fertilizer_Rec.html', x=x)
+
+@app.route('/predict-fertilizer', methods=['POST'])
+def predict_fertilizer():
+    try:
+        temp = request.form.get('temp')
+        humi = request.form.get('humid')
+        mois = request.form.get('mois')
+        soil = request.form.get('soil')
+        crop = request.form.get('crop')
+        nitro = request.form.get('nitro')
+        pota = request.form.get('pota')
+        phosp = request.form.get('phos')
+
+        inputs = [temp, humi, mois, soil, crop, nitro, pota, phosp]
+        if None in inputs or not all(val.isdigit() for val in inputs):
+            session['fertilizer_result'] = 'Invalid input. Please provide numeric values for all fields.'
+            return redirect(url_for('fertilizer_suggestion'))
+
+        input_values = [int(temp), int(humi), int(mois), int(soil), int(crop), int(nitro), int(pota), int(phosp)]
+
+        prediction = model.predict([input_values])
+        res = str(ferti.classes_[prediction[0]])
+
+        session['fertilizer_result'] = res
+        return redirect(url_for('fertilizer_suggestion'))
+
+    except Exception as e:
+        session['fertilizer_result'] = f"Error: {str(e)}"
+        return redirect(url_for('fertilizer_suggestion'))
+
 
 
 if __name__ == '__main__':
